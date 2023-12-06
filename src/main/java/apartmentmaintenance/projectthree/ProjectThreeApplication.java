12ba3c2bf47c1c4014a3d8a42af79cff71ec49cb
apmiller111@gmail.com <Bellum223&G4>
2023-12-06 14:07:43 -0500

    creation

---
+++ src/main/java/apartmentmaintenance/projectthree/ProjectThreeApplication.java
@@ -0,0 +1,13 @@
+package apartmentmaintenance.projectthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectThreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/apartmentmaintenance/projectthree/ProjectThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package apartmentmaintenance.projectthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectThreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
